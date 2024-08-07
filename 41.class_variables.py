9554e493edd4fa1ec2ba01b8e41f17fb37a736e0
michalkoperski <michal@koperski.cc>
2024-08-07 14:32:58 +0200

    1

--- 41.class_variables.py
+++ 41.class_variables.py
@@ -1,9 +1,10 @@
-from car import Car
+from car2 import Car
 
 car_1 = Car("Chevy","Corvette",2021,"blue")
 car_2 = Car("Ford","Mustang",2022,"red")
 
-print(car_1.make)
+print(car_1.wheels)
 
-car_1.drive()
-car_2.stop()
+Car.wheels = 2
+
+print(car_1.wheels)
